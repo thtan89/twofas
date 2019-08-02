184f8ede8088d7bdba18c3ffdf79ec4d019a0baa
Tan <thtan@tssb.com.my>
2019-08-02 09:38:02 +0800

    first commit

---
+++ twofas/urls.py
@@ -0,0 +1,36 @@
+"""twofas URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+from django_otp.admin import OTPAdminSite
+
+class OPTAdmin(OTPAdminSite):
+    pass
+
+from django.contrib.auth.models import User
+from django_otp.plugins.otp_totp.models import TOTPDevice
+
+admin_site = OPTAdmin(name='OTPAdmin')
+admin_site.register(User)
+admin_site.register(TOTPDevice)
+
+
+urlpatterns = [
+	url(r'^admin/', admin_site.urls),
+    url(r'^dadmin/', admin.site.urls),
+]
